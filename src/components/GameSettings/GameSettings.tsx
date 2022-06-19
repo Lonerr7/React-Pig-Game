992b45b1a7e060faca894b11336301d978e7e831
Lonerr7 <krabik2342@gmail.com>
2022-06-19 11:53:53 +0300

    4.2. Refactored code into MaxScore component

--- src/components/GameSettings/GameSettings.tsx
+++ src/components/GameSettings/GameSettings.tsx
@@ -1,4 +1,5 @@
 import s from './GameSettings.module.scss';
+import MaxScore from './MaxScore/MaxScore';
 
 type GameSettingsProps = {
   classItem?: string;
@@ -28,44 +29,17 @@ const GameSettings: React.FC<GameSettingsProps> = ({
       <button className={s.gameSettings__cross} onClick={onSettingsClose}>
         ✖
       </button>
-      <div className={s.gameSettings__scoreBox}>
-        <p className={s.gameSettings__maxScore}>Max score: {maxScore}</p>
-
-        <div className={s.gameSettings__scoreControls}>
-          {isInputVisible ? (
-            <>
-              <div className={s.gameSettings__scoreControlsBox}>
-                <input
-                  className={s.gameSettings__scoreInput}
-                  type="number"
-                  max={100}
-                  min={1}
-                  value={inputValue}
-                  onChange={onInputChange}
-                />
-                <button
-                  className={s.gameSettings__scoreSubmit}
-                  onClick={() => setNewMaxScore(inputValue)}
-                >
-                  ✔
-                </button>
-              </div>
-              {errorMessage ? (
-                <p className={s.gameSettings__scoreError}>{errorMessage}</p>
-              ) : (
-                ''
-              )}
-            </>
-          ) : (
-            <button
-              className={s.gameSettings__changeScoreBtn}
-              onClick={onScoreInputOpen}
-            >
-              Change Max Score
-            </button>
-          )}
-        </div>
-      </div>
+      <MaxScore
+        maxScore={maxScore}
+        otherProps={{
+          isInputVisible,
+          inputValue,
+          onInputChange,
+          onScoreInputOpen,
+          setNewMaxScore,
+          errorMessage,
+        }}
+      />
     </div>
   );
 };
--- src/components/GameSettings/MaxScore/MaxScore.tsx
+++ src/components/GameSettings/MaxScore/MaxScore.tsx
@@ -1,7 +1,63 @@
 import s from './MaxScore.module.scss';
+import { BsPencil } from 'react-icons/bs';
+import { TiTick } from 'react-icons/ti';
 
-const MaxScore: React.FC = () => {
-  return <div className={s.maxScore}>MaxScore</div>;
+type MaxScoreProps = {
+  maxScore: number;
+  otherProps: {
+    isInputVisible: boolean;
+    inputValue: number;
+    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onScoreInputOpen: () => void;
+    setNewMaxScore: (newScore: number) => void;
+    errorMessage: string;
+  };
+};
+
+const MaxScore: React.FC<MaxScoreProps> = ({ maxScore, otherProps }) => {
+  const {
+    isInputVisible,
+    onScoreInputOpen,
+    inputValue,
+    onInputChange,
+    setNewMaxScore,
+    errorMessage,
+  } = otherProps;
+
+  return (
+    <div className={s.maxScore}>
+      <p className={s.maxScore__title}>
+        Max Game Score: <span>{maxScore}</span>
+      </p>
+      {isInputVisible ? (
+        <div className={s.maxScore__inputBox}>
+          <input
+            className={s.maxScore__input}
+            type="number"
+            value={inputValue}
+            onChange={onInputChange}
+          />
+          <button
+            className={s.maxScore__inputBtn}
+            onClick={() => {
+              setNewMaxScore(inputValue);
+            }}
+          >
+            <TiTick size={20} />
+          </button>
+        </div>
+      ) : (
+        <button className={s.maxScore__btn} onClick={onScoreInputOpen}>
+          <BsPencil size={15} />
+        </button>
+      )}
+      {errorMessage ? (
+        <p className={s.maxScore__errorMessage}>{errorMessage}</p>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
 
 export default MaxScore;
